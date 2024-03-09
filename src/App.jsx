d18e98920d0d17e83549924a60ad4686fa53fb50
pratishu <pratishu.kr@gmail.com>
2024-03-09 16:43:22 +0530

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div></div>;
+  return <div>some thing here</div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import About from "./pages/About.jsx";
+import Articles from "./pages/Articles.jsx";
+import Contact from "./pages/Contact.jsx";
+import Profile from "./pages/Profile.jsx";
+import Home from "./pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="articles" element={<Articles />} />
+      <Route path="Contact  " element={<Contact />} />
+      <Route path="Profile" element={<Profile />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
